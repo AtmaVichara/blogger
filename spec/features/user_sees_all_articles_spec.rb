6476a20fbbf33df886f79aa4f6012fec28d0f2a7
Joe Jobes <jmrjobes@gmail.com>
2018-02-01 16:59:54 -0700

    Add all articles feature tests

---
+++ spec/features/user_sees_all_articles_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "user sees all articles" do
+  describe "they visit /articles" do
+    it "displays all articles" do
+      article_1 = Article.create!(title: "Breaking News on Unauthorized Cashews", body: "A recent influx of unauthorized cashews has spawned a frenzy among the public and a crashing in the stocks of cashews. By god the horror")
+      article_2 = Article.create!(title: "All Cashews Deported", body: "With the recent influx of unauthorized cashews there has been a massive undertaking for deportations of said cashews. As of now many have been deported back to salads and pies.")
+      visit '/articles'
+
+      expect(page).to have_content(article_1.title)
+      expect(page).to have_content(article_1.body)
+      expect(page).to have_content(article_2.title)
+      expect(page).to have_content(article_2.body)
+    end
+  end
+end
