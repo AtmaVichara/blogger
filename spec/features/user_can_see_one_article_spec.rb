58d59347fc635a4d2ddfa59cd9cb6ac2efe73420
Joe Jobes <jmrjobes@gmail.com>
2018-02-02 08:26:24 -0700

    Add show articles feature test

---
+++ spec/features/user_can_see_one_article_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "user can see one article" do
+  describe "they link from the articles index" do
+    it "displays information for one article" do
+      article_1 = Article.create!(title: "Breaking News on Unauthorized Cashews", body: "A recent influx of unauthorized cashews has spawned a frenzy among the public and a crashing in the stocks of cashews. By god the horror")
+
+      visit articles_path
+
+      click_link article.title
+
+      expect(page).to have_content(article_1.title)
+      expect(page).to have_content(article_1.description)
+      expect(page).to have_link("Back to All Articles")
+    end
+  end
+end
