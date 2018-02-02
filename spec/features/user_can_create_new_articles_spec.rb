2bb7b0c5c4946d9388df5e0deb2f3386afdcea28
Joe Jobes <jmrjobes@gmail.com>
2018-02-02 12:54:57 -0700

    Add in click_link in specs

--- spec/features/user_can_create_new_articles_spec.rb
+++ spec/features/user_can_create_new_articles_spec.rb
@@ -5,12 +5,13 @@ describe "user creates new articles" do
     describe "they fill in the title and body" do
       it "creates new article" do
         visit articles_path
-        click_on "Create a New Article"
+        click_link "Create a New Article"
 
         expect(current_path).to eq(new_article_path)
 
         fill_in "article[title]", with: "Unauthorized CASHEWS!!"
         fill_in "article[body]", with: "There are unauthorized cashews in the vacinity! Everyone be aware of these little monsters"
+        click_on "Create Article"
 
         expect(page).to have_content("Unauthorized CASHEWS!!")
         expect(page).to have_content("There are unauthorized cashews in the vacinity! Everyone be aware of these little monsters")
