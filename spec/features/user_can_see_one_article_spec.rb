8e79033264dbb575af765fea9ee64ea115406f78
Joe Jobes <jmrjobes@gmail.com>
2018-02-03 09:29:56 -0700

    Add to index feature showing comments

--- spec/features/user_can_see_one_article_spec.rb
+++ spec/features/user_can_see_one_article_spec.rb
@@ -4,6 +4,8 @@ describe "user can see one article" do
   describe "they link from the articles index" do
     it "displays information for one article" do
       article = Article.create!(title: "Breaking News on Unauthorized Cashews", body: "A recent influx of unauthorized cashews has spawned a frenzy among the public and a crashing in the stocks of cashews. By god the horror")
+      comment1 = article.comments.create(author_name: 'Grunkle Stan', body: "These are a menace on the community")
+      comment2 = article.comments.create(author_name: 'Josh Gumption', body: "I say let them live in peace!! They are tasty!")
 
       visit articles_path
 
@@ -12,8 +14,13 @@ describe "user can see one article" do
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
       expect(page).to have_link("Back to All Articles")
+      expect(page).to have_content(comment1.author_name)
+      expect(page).to have_content(comment1.body)
+      expect(page).to have_content(comment2.author_name)
+      expect(page).to have_content(comment2.body)
     end
   end
+
   describe "they click on back to all articles" do
     it "displays index page" do
       article = Article.create!(title: "Breaking News on Unauthorized Cashews", body: "A recent influx of unauthorized cashews has spawned a frenzy among the public and a crashing in the stocks of cashews. By god the horror")
@@ -25,5 +32,5 @@ describe "user can see one article" do
 
       expect(current_path).to eq('/articles')
     end
-  end 
+  end
 end
