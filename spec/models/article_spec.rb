9ee91d8a95ac7fa8eb0c5dc6e216f1cc7be82ac0
Joe Jobes <jmrjobes@gmail.com>
2018-02-04 10:09:31 -0700

    Add instance method test to article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,4 +6,15 @@ describe Article, type: :model do
     it { should validate_presence_of(:body) }
     it { should have_many(:comments) }
   end
+  describe "instance methods" do
+    describe "#tag_list" do
+      it "turns associated strings into tags" do
+        article = Article.create!(title: "They Have Arrived!", body: "The hyperdimensional starwarpers have appeared in a starbucks!!!")
+        article.tags.create(name: "invasion")
+        article.tags.create(name: "danger")
+
+        expect(article.tag_list).to eq("invasion, world danger")
+      end
+    end
+  end
 end
