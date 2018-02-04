eaed9208a982efeeb39e4713907682846364564e
Joe Jobes <jmrjobes@gmail.com>
2018-02-04 09:57:57 -0700

    Fix syntax errors with plurals and change tagging from has_many to belongs_to

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Tag, type: :model do
   describe 'relationships' do
-    it { should have_many(:tagging) }
-    it { should have_many(:article).through(:tagging) }
+    it { should have_many(:taggings) }
+    it { should have_many(:articles).through(:taggings) }
   end
 end
--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Tagging, type: :model do
   describe 'relationships' do
-    it { should have_many(:tag)}
-    it { should have_many(:article) }
+    it { should belong_to(:tag)}
+    it { should belong_to(:article) }
   end
 end
