317add596bd1117de1b5a369dd4cd4035b683e58
Joe Jobes <jmrjobes@gmail.com>
2018-02-04 16:42:33 -0700

    Add delete tag feature

---
+++ spec/features/tags/user_can_delete_tags_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'user deletes a tag' do
+  describe 'link from the show page' do
+    it 'displays all tags without the deleted tag' do
+      tag1 = Tag.create!(name: 'danger')
+      tag2 = Tag.create!(name: 'helpful')
+      tag3 = Tag.create!(name: 'spicy')
+
+      visit tag_path(tag1)
+      click_link "Delete"
+
+      expect(page).to have_content("You have Successfully Deleted the #{tag.name} tag")
+      expect(current_path).to eq(tags_path)
+      expect(page).to_not have_link(tag1.name)
+      expect(page).to have_link(tag2.name)
+      expect(page).to have_link(tag3.name)
+    end
+  end
+end
